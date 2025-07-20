736f33d03ea523a8b2ff11ba69ad1ff064a8faef
Jeff97ares <92552303+Jeff97ares@users.noreply.github.com>
2025-07-20 09:45:56 -0500

    fix: send plain password to auth-service to avoid double hashing

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,7 +19,7 @@ exports.registerUser = async (req, res) => {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         username,
-        password: hashedPassword
+        password
       })
     });
 
